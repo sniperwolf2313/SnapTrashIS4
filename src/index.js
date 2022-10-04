1124d01da225856c9588c5f6750ce986929c08cc
Michael Cardenas <maicol.cardenas2313@gmail.com>
2022-10-04 02:15:17 -0500

    creación repositorio

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require ('express')
+const app + express()
+
+//Settings
+app.set('port', process.env.PORT || 3000)
+
+//Middlewares
+
+//Global Variables
+
+//Routes
+
+//Static Files
+
+//Server is listening
+
+app.listen(app.get('port'))
+console.log('Server On Port ', app.get('port'))
